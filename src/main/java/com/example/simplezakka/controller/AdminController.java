102e9135333e29635e183acb61c6ea1e2e03a763
Rikumt12 <riku.yamashita@avantcorp.com>
2025-07-09 17:30:14 +0900

    admin追加

---
+++ src/main/java/com/example/simplezakka/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.example.simplezakka.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "admin"; // templates/admin.html を表示する
+    }
+}
