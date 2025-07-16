dff7be3b11e4baca29b4332b1d60b78df5420041
ryotarotoki <ryotaro.toki@avantcorp.com>
2025-07-16 12:14:58 +0900

    名称調整

--- src/main/java/com/example/simplezakka/entity/OrderItem.java
+++ src/main/java/com/example/simplezakka/entity/OrderItem.java
@@ -5,14 +5,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "order_details")
+@Table(name = "order_items")
 @Data
 @NoArgsConstructor
-public class OrderDetail {
+public class OrderItem {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer orderDetailId;
+    private Integer orderItemId;
     
     @ManyToOne
     @JoinColumn(name = "order_id", nullable = false)
